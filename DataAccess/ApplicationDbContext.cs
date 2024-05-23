133f8a4644f3d74510437a7cd165140b730157c6
SebastianKanizaj <skanizaj21@student.foi.hr>
2024-05-23 14:54:19 +0200

    Added the foreign keys to the database and addey the tools for the creation of the database and created a constructor in the ApplicationDbContext class

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -19,5 +19,58 @@ namespace DataAccess
         public DbSet<ProjectRole> ProjectRoles { get; set; }
         public DbSet<ProjectCategory> ProjectCategories { get; set; }
 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+
+            modelBuilder.Entity<WorkTime>()
+                .HasOne(w => w.User)
+                .WithMany(u => u.WorkTimes)
+                .HasForeignKey(w => w.UserId);
+
+            modelBuilder.Entity<ProjectTime>()
+                .HasKey(pt => new { pt.UserId, pt.ProjectId });
+
+            modelBuilder.Entity<ProjectTime>()
+                .HasOne(pt => pt.User)
+                .WithMany(u => u.ProjectTimes)
+                .HasForeignKey(pt => pt.UserId);
+
+            modelBuilder.Entity<ProjectTime>()
+                .HasOne(pt => pt.Project)
+                .WithMany(p => p.ProjectTimes)
+                .HasForeignKey(pt => pt.ProjectId);
+
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.ProjectCategory)
+                .WithMany(pc => pc.Projects)
+                .HasForeignKey(p => p.ProjectCategoryId);
+
+            modelBuilder.Entity<ProjectMember>()
+                .HasKey(pm => new { pm.UserId, pm.ProjectId, pm.ProjectRoleId });
+
+            modelBuilder.Entity<ProjectMember>()
+                .HasOne(pm => pm.User)
+                .WithMany(u => u.ProjectMembers)
+                .HasForeignKey(pm => pm.UserId);
+
+            modelBuilder.Entity<ProjectMember>()
+                .HasOne(pm => pm.Project)
+                .WithMany(p => p.ProjectMembers)
+                .HasForeignKey(pm => pm.ProjectId);
+
+            modelBuilder.Entity<ProjectMember>()
+                .HasOne(pm => pm.ProjectRole)
+                .WithMany(pr => pr.ProjectMembers)
+                .HasForeignKey(pm => pm.ProjectRoleId);
+        }
     }
 }
--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcacbb19264f677fb9ec63441945e1571708f152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d41ee68bf90b2339e48740a95a9b855a152931c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
